0eac51b9c9ae77422505c0e83b4db92f5deea578
Gaurav Agarwal <algogrit@gmail.com>
2019-06-03 16:07:52 +0530

    L04T01: Alternate solution.

--- Lesson-04/01 - PermCheck.rb
+++ Lesson-04/01 - PermCheck.rb
@@ -4,24 +4,14 @@ require 'pry'
 
 def solution(a)
   # write your code in Ruby 2.2
-  n = a.length
+  seen = {}
 
-  counter = Array.new(n, 0)
-  a_sum = 0
-
-  a.each do |elem|
-    a_sum += elem
-    return 0 if elem > n
-    current_count = counter[elem] || 0
-    return 0 if current_count > 0
-    counter[elem] = current_count + 1
+  max = a.max
+  a.each do |v|
+    seen[v] = true
   end
 
-  n_sum = n * (n+1) / 2
-
-  return 0 if n_sum != a_sum
-
-  return 1
+  seen.size == max && max == a.size ? 1 : 0
 end
 
 # binding.pry
