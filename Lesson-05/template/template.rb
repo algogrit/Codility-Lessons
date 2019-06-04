bd0d073166f9e150c1f6331b5dd3b8be29455a4e
Gaurav Agarwal <algogrit@gmail.com>
2019-06-04 23:29:04 +0530

    Adding template for future exercises.

---
+++ Lesson-05/template/template.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+require 'pry'
+require 'pp'
+require 'test/unit/assertions'
+
+include Test::Unit::Assertions
+
+def solution(a)
+end
+
+# binding.pry
+
+assert_equal(nil, solution([]))
