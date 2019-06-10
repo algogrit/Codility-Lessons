da6e6508d090685ece2c91e21a54af939fbbfe81
Gaurav Agarwal <algogrit@gmail.com>
2019-06-10 19:53:19 +0530

    Solution: L10T01 - CountFactors - O(n).

---
+++ Lesson-10/01 - CountFactors.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+
+require 'pry'
+require 'pp'
+require 'test/unit/assertions'
+
+include Test::Unit::Assertions
+
+def solution(n)
+  i = 1
+  result = 0
+  while i * i < n do
+    pp [i, n % i]
+    if n % i == 0
+      result += 2
+    end
+    i += 1
+  end
+  if i * i == n
+    result += 1
+  end
+  result
+end
+
+# binding.pry
+
+# assert_equal(8, solution(24))
+assert_equal(8, solution(42))
